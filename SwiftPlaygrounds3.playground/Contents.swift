3dcccfe833833b66ac2dee76d11bc0c3cd456602
CM Student <cmstudent@UHWO-ACM-C-05.local>
2018-02-12 10:52:00 -1000

    Added notes to SwiftPlaygrounds3/Assignment #4

--- SwiftPlaygrounds3.playground/Contents.swift
+++ SwiftPlaygrounds3.playground/Contents.swift
@@ -34,7 +34,7 @@ print(fruitBasket[1])
 //let's print out item at index 0
 
 //Part 8, Start of Assignment #4
-
+//How to use the type "Int"
 var numberLineup: [Int]
 
 numberLineup = [27,24,31,67,90]
@@ -42,9 +42,11 @@ numberLineup = [27,24,31,67,90]
 for number in numberLineup{
 print("My favorite number is " ,number)
 }
-
+//The Process on how to remove a number out of your array
 let removed = numberLineup.remove(at:2)
 print(numberLineup)
+
+//How to add an number into your array
 
 numberLineup.append(800)
 print(numberLineup)
